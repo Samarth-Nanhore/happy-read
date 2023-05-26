8b6abc52d58e8e93bd4fc31193d54e0ff61f6aae
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-05-26 20:28:37 +0530

    useReducer and array filter function are deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,24 @@ import { NavLink } from "react-router-dom";
 
 export const Home = () => {
   const { allBook, isLoding } = useContext(HomeContext);
-  console.log(allBook);
 
-  return (
-    <div>
-      {isLoding ? (
-        <p>Loding...</p>
-      ) : (
-        allBook.map((book) => {
-          const { author, _id, title, price, categoryName } = book;
-          return (
-            <div id="book-div">
-              <h3>Title: {title}</h3>
-              <p>Author: {author}</p>
-              <p>Price: {price}</p>
-              <NavLink to={`/about/${_id}`}>
-                <p>Book Detail</p>
-              </NavLink>
-            </div>
-          );
-        })
-      )}
-    </div>
-  );
+  const renderBook = () => {
+    return allBook.map((book) => {
+      const { author, _id, title, price, categoryName, rating } = book;
+      return (
+        <div id="book-div" key={_id}>
+          <h3>Title: {title}</h3>
+          <p>Author: {author}</p>
+          <p>Rating: {rating}</p>
+          <p>Price: {price}</p>
+
+          <NavLink to={`/about/${_id}`}>
+            <p>Book Detail</p>
+          </NavLink>
+        </div>
+      );
+    });
+  };
+
+  return <div>{isLoding ? <h3>...Loding</h3> : renderBook()}</div>;
 };
