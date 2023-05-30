24e4bb2d2a41801dcb62b941ecabe0d4621cb24c
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-05-31 02:21:47 +0530

    Add logout feature with redirect to login page

--- src/pages/user/userInfo.js
+++ src/pages/user/userInfo.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const UserInfo = () => {
   const [UserDetail, setUserDetail] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const userDetail = localStorage.getItem("userDetail");
@@ -16,6 +18,12 @@ export const UserInfo = () => {
   }, []);
   console.log(UserDetail);
 
+  const handleLogout = () => {
+    localStorage.removeItem("encodedToken");
+    localStorage.removeItem("userDetail");
+    navigate("/login");
+  };
+
   return (
     <>
       <h1>UserDetail</h1>
@@ -23,6 +31,7 @@ export const UserInfo = () => {
       <p>FirstName: {UserDetail?.firstName}</p>
       <p>LastName: {UserDetail?.lastName}</p>
       <p>Email: {UserDetail?.email}</p>
+      <button onClick={handleLogout}>Logout</button>
     </>
   );
 };
