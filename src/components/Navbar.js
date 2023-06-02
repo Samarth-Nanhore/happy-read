43e15e2a2bad1a8856e343c16b6e9269df4218ec
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-06-02 15:11:25 +0530

    Update styling of navbar and welcome page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import "../styles/Navbar.css"; // Import the CSS file
 
 export const Navbar = () => {
   const navigate = useNavigate();
@@ -13,17 +14,22 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav>
-        <NavLink to="/home">Home</NavLink>||
-        <NavLink to="/wishlist">WishList</NavLink>||
-        <NavLink to="/cart">Cart</NavLink>||
-        <span onClick={handleUserClick}>
-          {encodedToken ? (
-            <NavLink to="/userinfo">User</NavLink>
-          ) : (
-            <NavLink to="/login">Login</NavLink>
-          )}
-        </span>
+      <nav className="navbar">
+        <NavLink to="/" className="logo">
+          HappyRead
+        </NavLink>
+        <div className="nav-links">
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/wishlist">WishList</NavLink>
+          <NavLink to="/cart">Cart</NavLink>
+          <span onClick={handleUserClick}>
+            {encodedToken ? (
+              <NavLink to="/userinfo">User</NavLink>
+            ) : (
+              <NavLink to="/login">Login</NavLink>
+            )}
+          </span>
+        </div>
       </nav>
     </>
   );
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom";
+import "../styles/Welcome.css";
 
 export const Welcome = () => {
   return (
-    <>
+    <div className="banner">
       <h2>Welcome To Happy Read</h2>
-      <p>Get your all favourite books here </p>
-      <NavLink to="/home">
-        {" "}
-        <p>Shop Now</p>
+      <p>"Find Your Next Great Read."</p>
+      <NavLink to="/home" className="explore-button">
+        Explore Now
       </NavLink>
-    </>
+    </div>
   );
 };
